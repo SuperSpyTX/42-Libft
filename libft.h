27a704d26ca9b419a10e70d857702e997e1faa58
Joe <SuperSpyTX@users.noreply.github.com>
2017-10-06 00:41:38 -0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -21,7 +21,7 @@
 */
 int					ft_atoi(const char *str);
 
-//char				*ft_itoa(int n);
+char				*ft_itoa(int n);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memset(void *b, int c, size_t len);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
